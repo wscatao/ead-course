cbce49e07b0597cbc0ffd1ac9bf51944f020da79
Willy <wscatao@gmail.com>
2022-09-10 11:25:45 -0300

    Criado CourseDto com validacoes minimas e metodos crud na service.

--- src/main/java/com/ead/course/services/CourseService.java
+++ src/main/java/com/ead/course/services/CourseService.java
@@ -1,4 +1,18 @@
 package com.ead.course.services;
 
+import com.ead.course.models.CourseModel;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface CourseService {
+    CourseModel save(CourseModel courseModel);
+
+    Optional<CourseModel> findById(UUID courseId);
+
+    void delete(CourseModel courseModel);
+
+    List<CourseModel> findAll();
 }
+
--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,13 +1,39 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.CourseModel;
 import com.ead.course.repositories.CourseRepository;
 import com.ead.course.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class CourseServiceImpl implements CourseService {
 
     @Autowired
     CourseRepository courseRepository;
+
+    @Override
+    public CourseModel save(CourseModel courseModel) {
+        return courseRepository.save(courseModel);
+    }
+
+    @Override
+    public Optional<CourseModel> findById(UUID courseId) {
+        return courseRepository.findById(courseId);
+    }
+
+    @Override
+    public void delete(CourseModel courseModel) {
+        courseRepository.delete(courseModel);
+    }
+
+    @Override
+    public List<CourseModel> findAll() {
+
+        return courseRepository.findAll();
+    }
 }
