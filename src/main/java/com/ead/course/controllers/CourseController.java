7c71398166320b4322c782352833bc34447b407f
Willy <wscatao@gmail.com>
2022-09-22 10:00:03 -0300

    Preparado o controller para receber o userId para fazer o relacionamento de todos os cursos para um determinado usuario.

--- src/main/java/com/ead/course/controllers/CourseController.java
+++ src/main/java/com/ead/course/controllers/CourseController.java
@@ -21,14 +21,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
 @Log4j2
 @RestController
 @RequestMapping("/courses")
@@ -113,13 +114,19 @@ public class CourseController {
     @GetMapping
     public ResponseEntity<Page<CourseModel>> getAllCourses(
             SpecificationTemplate.CourseSpec spec,
-            @PageableDefault(page = 0, size = 10, sort = "name", direction = Sort.Direction.ASC) Pageable pageable
+            @PageableDefault(page = 0, size = 10, sort = "name", direction = Sort.Direction.ASC) Pageable pageable,
+            @RequestParam(required = false) UUID userId
     ) {
 
-        Page<CourseModel> courseModelPage = courseService.findAll(spec, pageable);
+        Page<CourseModel> courseModelPage = null;
+
+        if (userId != null) {
+            courseModelPage = courseService.findAll(SpecificationTemplate.courseUserId(userId).and(spec), pageable);
+        } else {
+            courseModelPage = courseService.findAll(spec, pageable);
+        }
 
         return ResponseEntity.status(HttpStatus.OK).body(courseModelPage);
-
     }
 
     @GetMapping("/{courseId}")
