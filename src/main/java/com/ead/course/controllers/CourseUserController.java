adfe3be0731bdf4521c25e25e3e6f8fb792d1f22
Willy <wscatao@gmail.com>
2022-10-27 09:09:14 -0300

    rota para deletar usuario no relacionamento de um curso.

--- src/main/java/com/ead/course/controllers/CourseUserController.java
+++ src/main/java/com/ead/course/controllers/CourseUserController.java
@@ -9,18 +9,12 @@ import com.ead.course.services.CourseService;
 import com.ead.course.services.CourseUserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpStatusCodeException;
 
 import javax.validation.Valid;
@@ -91,6 +85,18 @@ public class CourseUserController {
                         courseModelOptional.get().convertToCourseUserModel(subscriptionDto.getUserId()));
 
         return ResponseEntity.status(HttpStatus.CREATED).body(courseUserModel);
+    }
+
+    @DeleteMapping("/courses/users/{userId}")
+    public ResponseEntity<Object> deleteCourseUserByUser(@PathVariable(value = "userId") UUID userId) {
+
+        if(!courseUserService.existsByUserId(userId)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("CourseUser not found.");
+        }
+
+        courseUserService.deleteCourseUserByUser(userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body("CourseUser deleted successfully.");
     }
 
 }
